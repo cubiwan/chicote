dee59fb5d0a33973f47628ca7846f8ff56199c0a
Cubiwan <cubiwan@gmail.com>
2021-03-21 22:05:27 +0100

    Add files via upload

--- cooking.js
+++ cooking.js
@@ -1,4 +1,4 @@
-var input = require("./recipe.json");
+var recipes = require("./recipes.json");
 var know = require("./cookbook.json");
 
 var fs = require("fs");
@@ -11,100 +11,101 @@ var plural = eval(filedata);
 
 var v = require('./js/voca.js');
 
-let date = new Date();
-var counter = 0;
 
-input["timestamp"] = date.getTime();
-input["date"] = date.toISOString().split('T')[0];
-input["time"] = date.toISOString().split('T')[1].split('.')[0]
-input["GUID"] = createGuid();
+function addTools(input){
+	let date = new Date();
+	var counter = 0;
 
-input["plural"] = functionToTagAfterRender(plural);
-input["camel"] = functionToTagAfterRender(v.camelCase)
-input["capital"] = functionToTagAfterRender(v.capitalize);
-input["decapital"] = functionToTagAfterRender(v.decapitalize);
-input["dash"] = functionToTagAfterRender(v.kebabCase);
-input["snake"] = functionToTagAfterRender(v.snakeCase);
-input["swap"] = functionToTagAfterRender(v.swapCase);
-input["title"] = functionToTagAfterRender(v.titleCase);
-input["lower"] = functionToTagAfterRender(v.lowerCase);
-input["upper"] = functionToTagAfterRender(v.upperCase);
+	input["timestamp"] = date.getTime();
+	input["date"] = date.toISOString().split('T')[0];
+	input["time"] = date.toISOString().split('T')[1].split('.')[0]
+	input["GUID"] = createGuid();
 
-input["reverse"] = functionToTagAfterRender(v.reverse);
-input["stripTags"] = functionToTagAfterRender(v.stripTags);
-input["escHtml"] = functionToTagAfterRender(v.escapeHtml);
-input["unHtml"] = functionToTagAfterRender(v.unescapeHtml);
-input["escRegExp"] = functionToTagAfterRender(v.escapeRegExp);
-input["trim"] = functionToTagAfterRender(v.trim);
-input["latin"] = functionToTagAfterRender(v.latinise);
+	input["plural"] = functionToTagAfterRender(plural);
+	input["camel"] = functionToTagAfterRender(v.camelCase)
+	input["capital"] = functionToTagAfterRender(v.capitalize);
+	input["decapital"] = functionToTagAfterRender(v.decapitalize);
+	input["dash"] = functionToTagAfterRender(v.kebabCase);
+	input["snake"] = functionToTagAfterRender(v.snakeCase);
+	input["swap"] = functionToTagAfterRender(v.swapCase);
+	input["title"] = functionToTagAfterRender(v.titleCase);
+	input["lower"] = functionToTagAfterRender(v.lowerCase);
+	input["upper"] = functionToTagAfterRender(v.upperCase);
+	input["slug"] = functionToTagAfterRender(v.slugify);
+	input["reverse"] = functionToTagAfterRender(v.reverse);
 
-input["delSpaces"] = functionToTagAfterRender(delSpaces);
-input["delDuplicateSpaces"] = functionToTagAfterRender(delDuplicateSpaces);
+	input["count"] = functionToTagAfterRender(v.count);
+	input["countWords"] = functionToTagAfterRender(v.countWords);
 
-input["repeat2"] = functionToTagBeforeRender(repeat2);
-input["repeat3"] = functionToTagBeforeRender(repeat3);
-input["repeat4"] = functionToTagBeforeRender(repeat4);
-input["repeat5"] = functionToTagBeforeRender(repeat5);
-input["repeat6"] = functionToTagBeforeRender(repeat6);
-input["repeat7"] = functionToTagBeforeRender(repeat7);
-input["repeat8"] = functionToTagBeforeRender(repeat8);
-input["repeat9"] = functionToTagBeforeRender(repeat9);
+	input["stripTags"] = functionToTagAfterRender(v.stripTags);
+	input["escHtml"] = functionToTagAfterRender(v.escapeHtml);
+	input["unHtml"] = functionToTagAfterRender(v.unescapeHtml);
+	input["escRegExp"] = functionToTagAfterRender(v.escapeRegExp);
+	input["trim"] = functionToTagAfterRender(v.trim);
+	input["latin"] = functionToTagAfterRender(v.latinise);
 
-input["sortAscL"] = functionToTagAfterRender(sortAscL);
-input["sortDescL"] = functionToTagAfterRender(sortDescL);
-input["trimL"] = functionToTagAfterRender(trimL);
-input["joinL"] = functionToTagAfterRender(joinL);
-input["removeDuplicateL"] = functionToTagAfterRender(removeDuplicateL);
-input["spaceL"] = functionToTagAfterRender(spaceL);
-input["space2L"] = functionToTagAfterRender(space2L);
-input["space4L"] = functionToTagAfterRender(space4L);
-input["space8L"] = functionToTagAfterRender(space8L);
-input["tabL"] = functionToTagAfterRender(tabL);
-input["tab2L"] = functionToTagAfterRender(tab2L);
-input["tab3L"] = functionToTagAfterRender(tab3L);
-input["tab4L"] = functionToTagAfterRender(tab4L);
-input["tab5L"] = functionToTagAfterRender(tab5L);
-input["tab6L"] = functionToTagAfterRender(tab6L);
+	input["delSpaces"] = functionToTagAfterRender(delSpaces);
+	input["delDuplicateSpaces"] = functionToTagAfterRender(delDuplicateSpaces);
+	input["delLast"] = functionToTagAfterRender(delLast);
+	input["delFirst"] = functionToTagAfterRender(delFirst);
+	input["delEnd"] = functionToTagAfterRender(delEnd);
+	input["delStart"] = functionToTagAfterRender(delStart);
+
+	input["repeat"] = functionToTagBeforeRender(repeat);
+
+	input["delEmptyL"] = functionToTagAfterRender(delEmptyL);
+	input["sortAscL"] = functionToTagAfterRender(sortAscL);
+	input["sortDescL"] = functionToTagAfterRender(sortDescL);
+	input["trimL"] = functionToTagAfterRender(trimL);
+	input["joinL"] = functionToTagAfterRender(joinL);
+	input["delDuplicateL"] = functionToTagAfterRender(delDuplicateL);
+	input["spaceL"] = functionToTagAfterRender(spaceL);
+	input["tabL"] = functionToTagAfterRender(tabL);
+	input["addStartL"] = functionToTagAfterRender(addStartL);
+	input["addEndL"] = functionToTagAfterRender(addEndL);
 
 
-input["log"] = functionToTagAfterRender(console.log);
+	input["log"] = functionToTagAfterRender(log);
 
-input["K"] = function() {
-	return function (text, render) {
-		return render(know[render(text)]);
+	input["K"] = function() {
+		return function (text, render) {
+			return render(know[render(text)]);
+		};
 	};
-};
-input["R"] = function() {
-	return function (text, render) {			
-		return render(text);
-	};
+	input["R"] = function() {
+		return function (text, render) {			
+			return render(text);
+		};
+	}
+
+	input["C="] = functionToTagAfterRender(resetCounter);
+	input["C+"] = functionToTagAfterRender(incCounter);
+	input["C-"] = functionToTagAfterRender(decCounter);
+	input["C"] = functionToTagAfterRender(writeCounter);
 }
 
-input["C0"] = functionToTagAfterRender(resetCounter);
-input["C+"] = functionToTagAfterRender(incCounter);
-input["C-"] = functionToTagAfterRender(decCounter);
-input["C"] = functionToTagAfterRender(writeCounter);
-
-function resetCounter(text){	
-	counter = 0;
+function resetCounter(text, params){	
+	params = params | 0;
+	counter = params;
 	return text;
 }
 
-function incCounter(text){
-	counter++;
+function incCounter(text, params){
+	params = params | 1;
+	counter += params;
 	return text;
 	
 }
 
-function writeCounter(text){	
-	return counter+text;	
-}
-
-function decCounter(text){
-	counter--;
+function decCounter(text, params){
+	params = params | 1;
+	counter -= params;
 	return render(text);	
 }
 
+function writeCounter(text){	
+	return counter+""+text;	
+}
 
 function sortAscL(text){	
 	let lines = getLines(text);
@@ -129,73 +130,82 @@ function trimL(text){
 	return joinLines(lines);
 }
 
-function removeDuplicateL(text){
+function delDuplicateL(text){
 	let lines = getLines(text);
 	return lines.filter((item, i, allItems) => {
 		return i === allItems.indexOf(item);
-	})
-	.join("\n");
+	}).join("\n");
+}
+
+function delEmptyL(text){
+	let lines = getLines(text);
+	return lines.filter((item, i, allItems) => {
+		return item != "";
+	}).join("\n")+"\n";
 }
 
 function spaceL(text){	
 	let lines = getLines(text);
-	lines = lines.map(x => " "+x.trim());
+	let prefix = "";
+	for(let i = 0; i < param; i++){
+		prefix += " ";
+	}
+	lines = lines.map(x => prefix+x.trim());
 	return joinLines(lines);
 }
 
-function space2L(text){	
+function tabL(text, param){	
 	let lines = getLines(text);
-	lines = lines.map(x => "  "+x.trim());
+	let prefix = "";
+	for(let i = 0; i < param; i++){
+		prefix += "\t";
+	}
+	lines = lines.map(x => prefix+x.trim());
 	return joinLines(lines);
 }
 
-function space4L(text){	
+function delLast(text, param){
+	let index;
+	index = text.lastIndexOf(param);
+	if(index > -1){
+		text = text.slice(0, index)+""+text.substring(index+param.length);
+	}
+
+	return text;
+}
+
+function delFirst(text, param){
+	let index;
+	index = text.indexOf(param);
+	if(index > -1){
+		text = text.slice(0, index)+""+text.substring(index+param.length);
+	}
+
+	return text;
+}
+
+function delEnd(text, param){
+	return text.slice(0, -param);
+}
+
+function delStart(text, param){
+	return text.substring(param);
+}
+
+
+function addStartL(text, params){	
 	let lines = getLines(text);
-	lines = lines.map(x => "    "+x.trim());
+	lines = lines.map(x => params+""+x);
 	return joinLines(lines);
 }
 
-function space8L(text){	
+function addEndL(text, params){	
 	let lines = getLines(text);
-	lines = lines.map(x => "        "+x.trim());
+	lines = lines.map(x => x+""+params);
 	return joinLines(lines);
 }
 
-function tabL(text){	
-	let lines = getLines(text);
-	lines = lines.map(x => "\t"+x.trim());
-	return joinLines(lines);
-}
 
-function tab2L(text){	
-	let lines = getLines(text);
-	lines = lines.map(x => "\t\t"+x.trim());
-	return joinLines(lines);
-}
-
-function tab3L(text){	
-	let lines = getLines(text);
-	lines = lines.map(x => "\t\t\t"+x.trim());
-	return joinLines(lines);
-}
-
-function tab4L(text){	
-	let lines = getLines(text);
-	lines = lines.map(x => "\t\t\t\t"+x.trim());
-	return joinLines(lines);
-}
-
-function tab5L(text){	
-	let lines = getLines(text);
-	lines = lines.map(x => "\t\t\t\t\t"+x.trim());
-	return joinLines(lines);
-}
-
-function tab6L(text){	
-	let lines = getLines(text);
-	lines = lines.map(x => "\t\t\t\t\t\t"+x.trim());
-	return joinLines(lines);
-}
 
 
 function getLines(text){
@@ -223,67 +233,38 @@ function delDuplicateSpaces(text){
 	return text.replace(/\s+/g, ' ');
 }
 
-
-function repeat2(text){	
-	return text+""+text;
+function repeat(text, param){
+	let result = "";
+	for(let i = param; i > 0; i--){	
+		result += ""+text;
+	}
+	return result 
 }
 
-function repeat3(text){
-	return text+""+repeat2(text);
+function log(text){
+	console.log(text);
 }
-
-function repeat4(text){
-	return text+""+repeat3(text);
-}
-
-function repeat5(text){
-	return text+""+repeat4(text);
-}
-
-function repeat6(text){
-	return text+""+repeat5(text);
-}
-
-function repeat7(text){
-	return text+""+repeat6(text);
-}
-
-function repeat8(text){
-	return text+""+repeat7(text);
-}
-
-function repeat9(text){
-	return text+""+repeat8(text);
-}
-
 
 
 function functionToTagAfterRender(f){
 	return function () {
-		return function (text, render) {			
-			return f(render(text))
+		return function (text, render, param) {
+			return f(render(text), param)
 		};
 	}
 }
 
 function functionToTagBeforeRender(f){
 	return function () {
-		return function (text, render) {			
-			return render(f(text))
+		return function (text, render, param) {
+			return render(f(text, param))
 		};
 	}
 }
 
 
-
 Mustache.escape = function(text) {return text;};
 
-var step = [];
-var vars = [];
-var templates = [];
-var directories = [];
-var dirs = [];
-var paths = [];
 
 console.log("Del output");
 rmdir("./output/");
@@ -291,55 +272,67 @@ rmdir("./output/");
 console.log("Creating dir: output");
 fs.mkdirSync("./output");
 
-for(index in input.steps){	
-	step = require("./steps/"+ input.steps[index]+".json");
-	if(step.templates) {
-		templates = templates.concat(step.templates);
+for(recipeIndex in recipes){
+	var recipe = recipes[recipeIndex];
+
+	addTools(recipe);
+
+	var step = [];
+	var vars = [];
+	var templates = [];
+	var directories = [];
+	var dirs = [];
+	var paths = [];
+
+	for(index in recipe.steps){	
+		step = require("./steps/"+ recipe.steps[index]+".json");
+		if(step.templates) {
+			templates = templates.concat(step.templates);
+		}
+		if(step.directories) {
+			dirs = dirs.concat(step.directories);
+		}
+		if(step.vars) {
+			vars = vars.concat(step.vars)
+		}
 	}
-	if(step.directories) {
-		dirs = dirs.concat(step.directories);
+
+	for (index in vars){
+		recipe[vars[index][0]] = Mustache.render(vars[index][1], recipe);	
 	}
-	if(step.vars) {
-		vars = vars.concat(step.vars)
+
+	for (index in dirs) {
+		directories = directories.concat(extractPaths(dirs[index]));
 	}
-}
 
-for (index in vars){
-	input[vars[index][0]] = Mustache.render(vars[index][1], input);	
-}
-
-for (index in dirs) {
-	directories = directories.concat(extractPaths(dirs[index]));
-}
-
-for (index in directories) {
-	paths[directories[index]] = Mustache.render(directories[index], input);
-	if (!fs.existsSync("./output/"+paths[directories[index]])) {
-		console.log("Creating dir: "+paths[directories[index]]); 
-		fs.mkdirSync("./output/"+paths[directories[index]]);
+	for (index in directories) {
+		paths[directories[index]] = Mustache.render(directories[index], recipe);
+		if (!fs.existsSync("./output/"+paths[directories[index]])) {
+			console.log("Creating dir: "+paths[directories[index]]); 
+			fs.mkdirSync("./output/"+paths[directories[index]]);
+		}
 	}
-}
 
-for (index in templates){	
-	fs.readFile("./templates/"+templates[index][0], "utf8", parserTemplate(index));
-}
-
-return;
-//END
-
-function parserTemplate(index){
-	return function(err, file){ 
-		var result = Mustache.render(file, input);
-		var filename = Mustache.render(templates[index][1], input);
+	for (index in templates){	
+		var file = fs.readFileSync("./templates/"+templates[index][0], "utf8");
+		var render = Mustache.render(file, recipe);
+		var filename = Mustache.render(templates[index][1], recipe);
 		console.log("Generating: " + filename);
-		fs.writeFileSync("./output/"+filename, result, 'utf-8', function(err) {
+		fs.writeFileSync("./output/"+filename, render, 'utf-8', function(err) {
 			if(err) {
 				console.log("Error saving "+filename+" -> "+err);
 				process.exit(1);
 			}
-		}); 	
+		}); 
 	}
+
 }
+
+console.log("Bon Appetite!!!");
+
+return;
+//END
+
 
 function rmdir(pathDir) {
     var files = [];
